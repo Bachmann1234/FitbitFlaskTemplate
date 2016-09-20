b45ac863bcec7bcfbd8562dc33c398475b556c76
Matt Bachmann <bachmann.matt@gmail.com>
2016-09-19 22:51:27 -0400

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+#Fitbit OAuth Flask Template
+
+This project defines a basic structure to make applications based
+on the Fitbit API.
+
+It defines an app structure that will give you a database, a login form, and 
+a very basic registration form.
+
+The Flask Structure is from [Miguel Grinberg's Book](http://shop.oreilly.com/product/0636920031116.do) 
+
+If you wish to learn more about Flask check it out. Or the [Flask Project Page](http://flask.pocoo.org/)
+ 
+[Read More About the Fitbit API](https://dev.fitbit.com/docs/)
---
+++ app/models.py
@@ -0,0 +1,25 @@
+from flask.ext.login import UserMixin
+from werkzeug.security import generate_password_hash
+
+from app import db
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, username, password):
+        super(User, self).__init__()
+        self.username = username
+        self.password = password
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
